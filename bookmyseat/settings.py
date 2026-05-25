287ed569bacd130478539b40e1e5bc2293deb8a4
tarak456 <tarakaratna08@gmail.com>
2026-05-25 11:30:27 +0530

    Fix: use absolute path for templates

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -47,7 +47,7 @@ LOGIN_URL        = '/login/'
 
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': ['templates'],
+    'DIRS': [BASE_DIR / 'templates'],
     'APP_DIRS': True,
     'OPTIONS': {'context_processors': [
         'django.template.context_processors.debug',
