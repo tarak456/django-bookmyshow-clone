49397a0360f3e3187e2b1231433f4020ff012a89
tarak456 <tarakaratna08@gmail.com>
2026-05-26 10:55:39 +0530

    auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create superuser if it doesn't exist (free alternative to shell)
+python manage.py shell << 'PYEOF'
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@bookmyseat.com', 'Admin@1234')
+    print('Superuser created: admin / Admin@1234')
+else:
+    print('Superuser already exists')
+PYEOF
